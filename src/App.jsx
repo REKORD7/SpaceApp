ce2b35eca7591d670a9537c6666ebce44b813c5c
REKORD7 <godm8905@gmail.com>
2024-06-11 21:52:55 -0700

    Act aula 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
-
+import Cabecera from "./components/Cabecera"
+import BarraLateral from "./components/BarraLateral"
+import Banner from "./components/Banner"
+import banner from "./assets/banner.png"
 
 const FondoGradiente = styled.div`
   background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -9,12 +12,15 @@ const FondoGradiente = styled.div`
 `
 
 function App() {
-  
+
 
   return (
     <>
       <FondoGradiente>
-        <GlobalStyles/>
+        <GlobalStyles />
+        <Cabecera />
+        <BarraLateral />
+        <Banner texto="La galería más completa de fotos del espacio" backgroundImage={banner} />
       </FondoGradiente>
     </>
   )
